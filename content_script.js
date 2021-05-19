946b274997cfed6020aaffd3589e7e24388afa0b
das08 <takeda5955@gmail.com>
2021-05-19 11:15:01 +0900

    initial commit

---
+++ content_script.js
@@ -0,0 +1,125 @@
+class Video {
+    constructor(partnerID, partnerID2 ,entryID) {
+        this.partnerID = partnerID;
+        this.partnerID2 = partnerID2;
+        this.entryID = entryID;
+    }
+
+    async getVideoInfo(){
+        return await fetchVideoMetaData(this);
+    }
+}
+
+class VideoInfo {
+    constructor(videoName, duration, url) {
+        this.videoName = videoName;
+        this.duration = duration;
+        this.url = url;
+    }
+}
+
+function fetchVideoMetaData(video) {
+    const queryURL = `https://cdnapi.kaltura.com/api_v3/index.php?apiVersion=3.1.5&format=1&service=multirequest&1%3Aexpiry=86400&1%3Aservice=session&1%3Aaction=startWidgetSession&1%3AwidgetId=_${video.partnerID}&2%3Aaction=get&2%3AentryId=${video.entryID}&2%3Aservice=baseentry&2%3Aks=%7B1%3Aresult%3Aks%7D&2%3AresponseProfile%3Afields=createdAt%2CdataUrl%2Cduration%2Cname%2Cplays%2CthumbnailUrl%2CuserId&2%3AresponseProfile%3Atype=1&3%3Aaction=getbyentryid&3%3AentryId=${video.entryID}&3%3Aservice=flavorAsset&3%3Aks=%7B1%3Aresult%3Aks%7D`
+    const request = new XMLHttpRequest();
+    request.open("GET", queryURL);
+    request.responseType = "json";
+
+    return new Promise((resolve, reject) => {
+        request.addEventListener("load", (e) => {
+            const res = request.response;
+            if (!res) {
+                console.log("404 kadai data not found");
+                reject([new VideoInfo(null, null, null)]);
+            }
+            else {
+                const videoName = res[1].name;
+                const duration = res[1].duration;
+
+                let minBitRate = -1;
+                let flavorID = "";
+                for (let video of res[2]){
+                    if (video.bitrate > minBitRate){
+                        flavorID = video.id;
+                        minBitRate = video.bitrate;
+                    }
+                }
+
+                if (minBitRate !== -1){
+                    const url = `http://cdnapi.kaltura.com/p/${video.partnerID}/sp/${video.partnerID2}/playManifest/entryId/${video.entryID}/format/url/protocol/http/flavorId/${flavorID}`;
+                    resolve(new VideoInfo(videoName, duration, url));
+                } else {
+                    reject(new VideoInfo(null, null, null));
+                }
+
+            }
+        });
+        request.send();
+    });
+}
+
+
+
+let forEach = Array.prototype.forEach;
+let speed = 1.0;
+
+
+
+async function main(document) {
+    window.onload = () => {
+        initialize(window.document);
+    };
+    if (document) {
+        if (document.readyState === "complete") {
+            initialize(document);
+        } else {
+            document.onreadystatechange = () => {
+                if (document.readyState === "complete") {
+                    initialize(document);
+                }
+            };
+        }
+    }
+    // await fetchVideoMetaData();
+}
+
+
+const wait = (millisec) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, millisec);
+    });
+};
+
+async function initialize(document) {
+    let frameTags = document.getElementsByTagName("iframe");
+    forEach.call(frameTags, function (frame) {
+        try {
+            var childDocument = frame.contentDocument;
+        } catch (e) {
+            return;
+        }
+        main(childDocument);
+    });
+    let img = document.querySelectorAll('img');
+    console.log("img", img)
+
+    let videoIDList = [];
+
+
+    for (let i of img) {
+        const regex = i.src.match("https?://cfvod.kaltura.com/p/([^/]+)/sp/([^/]+)/thumbnail/entry_id/([^/]+)");
+        if(regex && regex.length === 4){
+            console.log("id ", regex[1], " id2 ", regex[2],  " entryid ", regex[3]);
+            videoIDList.push(new Video(regex[1], regex[2], regex[3]));
+        }
+    }
+
+    for (let v of videoIDList){
+        let res = await v.getVideoInfo();
+        console.log("url", res)
+    }
+
+
+
+}
+
+main();
